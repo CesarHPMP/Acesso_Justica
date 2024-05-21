868a10eb9bbbbe3e292f7678b48144477b52610a
CesarHPMP <cesarmelo812@gmail.com>
2024-05-21 18:58:53 -0300

    Abordagem de pontos a serem resolvido durante desenvolvimento.

---
+++ src/mainpython.py
@@ -0,0 +1,11 @@
+# main para parseador em python, passos seguem:
+
+#import ou criar tabela hash para maior velocidade de processsamento e para filtragem baseada em frequencia 
+#em que os tokens aparecem 
+
+#problemas a serem resolvidos: 
+# - Hash apenas guarda os tokens e suas frequencias mas nao onde estao no arquivo(Talvez guardar em classe como metodo ou dado ex: token.posicoes(token))
+# - Guardar local em que dado se encontra dentro do arquivo e retornanr para usuario como resultado de pesquisa.
+# - avaliacao correta de importancia para melhores resultados (frequencia do token em um dado arquivo e ou quao perto do inicio este aparece?)
+
+
