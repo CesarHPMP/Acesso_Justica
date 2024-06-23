d166d6b1dfc40d812b26fc12f7adf15089436a4a
CesarHPMP <cesarmelo812@gmail.com>
2024-06-23 20:31:38 -0300

    Mudando algumas coisas.

--- myproject/Site_AJ/asgi.py
+++ myproject/Site_AJ/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for myproject project.
+ASGI config for Site_AJ project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Site_AJ.settings')
 
 application = get_asgi_application()
--- myproject/Site_AJ/urls.py
+++ myproject/Site_AJ/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for Site_AJ project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
